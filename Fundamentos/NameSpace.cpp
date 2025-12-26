18928343da9b9fcbf74cd7d037a670ea3370e777
syspone <cmsouzaac@outlook.com>
2025-12-25 22:47:46 -0500

    incluindo arquivo Namespace

---
+++ Fundamentos/NameSpace.cpp
@@ -0,0 +1,17 @@
+//
+// Created by cmsouza on 25/12/2025.
+//
+
+#include "NameSpace.h"
+
+#include <iostream>
+#include <ostream>
+using namespace std;
+
+int main() {
+
+    // scope resolution operator
+    std::cout << "Good Morning!" << std::endl;
+    cout << "Bom dia, meu caro vingador!!!" << endl;
+    return 0;
+}
---
+++ Fundamentos/NameSpace.h
@@ -0,0 +1,13 @@
+//
+// Created by cmsouza on 25/12/2025.
+//
+
+#ifndef CURSO_NAMESPACE_H
+#define CURSO_NAMESPACE_H
+
+
+class NameSpace {
+};
+
+
+#endif //CURSO_NAMESPACE_H
